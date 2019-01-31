3aafff0ea8e60dcf0ffbe503fbada8c07e12d563
Nick <nick.richardson@rezonence.com>
2019-01-31 09:57:04 +0000

    Add login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PigeonDatatableComponent} from './pigeon-datatable/pigeon-datatable.component';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'pigeon/:id',
     component: PigeonDatatableComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
