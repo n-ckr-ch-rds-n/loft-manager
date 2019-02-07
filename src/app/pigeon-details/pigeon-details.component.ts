e7db822f6982ae18bf96781c9829c013f4bae6f9
Nick <nick.richardson@rezonence.com>
2019-02-07 16:00:04 +0000

    Add buttons to pigeon detail component

--- src/app/pigeon-details/pigeon-details.component.ts
+++ src/app/pigeon-details/pigeon-details.component.ts
@@ -7,15 +7,14 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
   templateUrl: './pigeon-details.component.html',
   styleUrls: ['./pigeon-details.component.scss']
 })
-export class PigeonDetailsComponent implements OnInit {
-  selectedPigeon: Pigeon;
+export class PigeonDetailsComponent{
 
   constructor(public dialogRef: MatDialogRef<PigeonDetailsComponent>,
               @Inject(MAT_DIALOG_DATA) public data: {selectedPigeon: Pigeon},
   ) { }
 
-  ngOnInit() {
-    console.log(this.selectedPigeon);
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
