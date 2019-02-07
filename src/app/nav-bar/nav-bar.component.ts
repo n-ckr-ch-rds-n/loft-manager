9fe4a0a47f9204fec7fc28ded160121b770a9a28
Nick <nick.richardson@rezonence.com>
2019-02-07 17:50:38 +0000

    Remove dead code

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {MatDialog} from '@angular/material';
-import {AddPigeonComponent} from '../add-pigeon/add-pigeon.component';
-import {RemovePigeonComponent} from '../remove-pigeon/remove-pigeon.component';
-import {Pigeon} from '../pigeon';
-import {NavEvent} from '../nav.event';
-import {EditPigeonComponent} from '../edit-pigeon/edit-pigeon.component';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth.service';
 
 @Component({
@@ -13,30 +7,7 @@ import {AuthService} from '../services/auth.service';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-  navBarOptions: Record<string, any> = {
-    [NavEvent.add]: AddPigeonComponent,
-    [NavEvent.remove]: RemovePigeonComponent,
-    [NavEvent.edit]: EditPigeonComponent
-  };
-
-  @Input()
-  selectedPigeon: Pigeon;
-
-  constructor(public dialog: MatDialog,
-              private auth: AuthService) { }
-
-  select(eventType: NavEvent) {
-    const dialogRef = this.dialog.open(this.navBarOptions[eventType], {
-      width: 'auto',
-      data: { selectedPigeon: this.selectedPigeon }
-    });
-
-    dialogRef.afterClosed().subscribe(() => {});
-  }
-
-  logout() {
-    this.auth.logout();
-  }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
