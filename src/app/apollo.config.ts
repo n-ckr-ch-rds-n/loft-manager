d8c3220febb5dd8adcba918d54e162c3d093739f
Nick <nick.richardson@rezonence.com>
2019-02-14 09:41:35 +0000

    Attach node token to http requests

--- src/app/apollo.config.ts
+++ src/app/apollo.config.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HttpHeaders} from '@angular/common/http';
 import {Apollo, ApolloModule} from 'apollo-angular';
 import {HttpLink, HttpLinkModule} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {getOperationAST} from '../../node_modules/graphql';
+import {ApolloLink} from 'apollo-link';
 
 @NgModule({
   exports: [
@@ -18,8 +19,18 @@ export class GraphQLModule {
     const uri = 'https://api.graph.cool/simple/v1/cjrahl4l55q080115r0djemfn';
     const http = httpLink.create({ uri });
 
+    const middleware = new ApolloLink((operation, forward) => {
+      const token = localStorage.getItem('userToken');
+      if (token) {
+        operation.setContext({
+          headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+        });
+      }
+      return forward(operation);
+    });
+
     apollo.create({
-      link: http,
+      link: middleware.concat(http),
       cache: new InMemoryCache()
     });
 
