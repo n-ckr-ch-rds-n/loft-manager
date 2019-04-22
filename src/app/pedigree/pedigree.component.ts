0ad0144cb8b60b737cfde9cdc465cbc95f18e655
Nick <nick.richardson@rezonence.com>
2019-04-22 18:26:52 +0100

    Implement pedigree flowchart using mermaid

--- src/app/pedigree/pedigree.component.ts
+++ src/app/pedigree/pedigree.component.ts
@@ -1,20 +1,46 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {AfterContentInit, Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
 import {Pigeon} from '../pigeon';
+import mermaid from 'mermaid';
 
 @Component({
   selector: 'app-pedigree',
   templateUrl: './pedigree.component.html',
   styleUrls: ['./pedigree.component.scss']
 })
-export class PedigreeComponent implements OnInit {
+export class PedigreeComponent implements AfterContentInit {
 
   constructor(public dialogRef: MatDialogRef<PedigreeComponent>,
               @Inject(MAT_DIALOG_DATA) public data: {selectedPigeon: Pigeon},
               public dialog: MatDialog) { }
 
-  ngOnInit() {
+  @ViewChild('mermaid')
+  public mermaidDiv;
 
+  public ngAfterContentInit(): void {
+    mermaid.initialize({
+      theme: 'dark'
+    });
+
+    const element: any = this.mermaidDiv.nativeElement;
+    const graphDefinition = 'graph LR' +
+      '\ngreat-grand-sire-->grand-sire' +
+      '\ngreat-grand-dam-->grand-sire' +
+      '\ngreat-grand-sire2-->grand-dam' +
+      '\ngreat-grand-dam2-->grand-dam' +
+      '\ngreat-grand-sire3-->grand-sire2' +
+      '\ngreat-grand-dam3-->grand-sire2' +
+      '\ngreat-grand-sire4-->grand-dam2' +
+      '\ngreat-grand-dam4-->grand-dam2' +
+      '\ngrand-sire-->sire' +
+      '\ngrand-dam-->sire' +
+      '\ngrand-sire2-->dam' +
+      '\ngrand-dam2-->dam' +
+      '\nsire-->pigeon' +
+      '\ndam-->pigeon';
+    mermaid.render('graphDiv', graphDefinition, (svgCode, bindFunctions) => {
+      element.innerHTML = svgCode;
+    });
   }
 
   exit(): void {
