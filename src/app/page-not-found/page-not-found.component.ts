ff4b8f0e528cbab19c5ec8cb732508d9959b96c7
nick_richardson <nick.richardson@rezonence.com>
2019-05-09 15:51:44 +0100

    Create 404 page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-pagenotfound',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(public snackBar: MatSnackBar,
+              public router: Router) { }
 
   ngOnInit() {
+    const snackBarRef = this.snackBar.open('I\'m sorry, that page doesn\'t exist', 'OK');
+    snackBarRef.onAction().subscribe(async () => await this.router.navigate(['/login']))
   }
 
 }
